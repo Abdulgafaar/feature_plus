af77f35e04e9ff9d61e0dfb4232f6a993d7125e7
Muizudeen Kusimo <devboyng@gmail.com>
2015-08-15 10:36:10 -0400

    Add Unit Test for Feature model

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from __future__ import unicode_literals
 
-# Create your tests here.
+from unittest2 import TestCase
+from ..factories import FeatureFactory
+
+
+class FeatureTestCase(TestCase):
+    """
+    Test Feature model
+    """
+
+    def test_unicode_is_correctly_returned(self):
+        feature = FeatureFactory(title='Draggable masthead')
+        self.assertEqual(unicode(feature), "{}".format(feature.title))
