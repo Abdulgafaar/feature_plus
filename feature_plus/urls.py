bd0015814bf705d0f76f6b552b15db0cf8e176c0
Muizudeen Kusimo <devboyng@gmail.com>
2015-08-16 08:13:19 -0400

    Add constant to app/models + update urls for Grappelli + exclude tests folder from linting

--- feature_plus/urls.py
+++ feature_plus/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'FeaturePlus.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
