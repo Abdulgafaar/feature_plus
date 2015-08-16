c559109e3ab7ea4335519a0d5615cc772da9c92f
Muizudeen Kusimo <devboyng@gmail.com>
2015-08-16 18:58:13 -0400

    Call update_priority logic from admin page

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Feature
+from .models import Feature, PRIORITY_ONE
+from .services import update_other_priorities
 
 
 class FeatureAdmin(admin.ModelAdmin):
     model = Feature
+
+    def save_model(self, request, obj, form, change):
+        if obj.priority == PRIORITY_ONE:
+            update_other_priorities(obj)
+            super(FeatureAdmin, self).save_model(request, obj, form, change)
 
 
 admin.site.register(Feature, FeatureAdmin)
