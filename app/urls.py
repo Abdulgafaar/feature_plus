76f59697a326e307958977bcc90e00182712868e
Muizudeen Kusimo <devboyng@gmail.com>
2015-08-16 11:26:46 -0400

    Update urls and views w.i.p

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import features
+from .views import features_add, features_list, features_delete
 
 urlpatterns = [
-    url(r'^$', features, name='features_view'),
+    url(r'^/add$', features_add, name='features_add'),
+    url(r'^/list$', features_list, name='features_list'),
+    url(r'^/delete$', features_delete, name='features_delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,46 @@
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
 
+from .forms import FeatureForm
+from .models import Feature
 
-def features(request):
+
+@csrf_protect
+def features_add(request):
     """
-    Handles CRUD operations for the Bootstrap template
+    Add/Edit Feature
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        feature_form = FeatureForm(request.POST)
+        if feature_form.is_valid():
+            HttpResponseRedirect('/list')
+    else:
+        feature_form = FeatureForm()
+
+    return render_to_response('features.html', {'feature_form': feature_form})
+
+
+def features_list(request):
+    """
+    Return List of available features
     :param request:
     :return:
     """
     template = loader.get_template('features.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render({}))
+
+
+def features_delete(request):
+    """
+    Return List of available features
+    :param request:
+    :return:
+    """
+
+    return HttpResponse({})
