fde21449bf8813ca4764749b42e9170b8e847af9
Muizudeen Kusimo <devboyng@gmail.com>
2015-08-23 14:05:55 -0400

    Add Unit Tests

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals, absolute_import
 
 from unittest2 import TestCase
 from ..factories import FeatureFactory
+from ..models import Feature
 
 
 class FeatureTestCase(TestCase):
@@ -9,6 +10,15 @@ class FeatureTestCase(TestCase):
     Test Feature model
     """
 
+    def setUp(self):
+        Feature.objects.all().delete()
+
     def test_unicode_is_correctly_returned(self):
         feature = FeatureFactory(title='Draggable masthead')
         self.assertEqual(unicode(feature), "{}".format(feature.title))
+
+    def test_uniqueness_is_enforced_when_creating_models(self):
+        feature1 = FeatureFactory.create(title='A', clients='Client B', priority=1)
+        self.assertTrue(
+            Feature.objects.filter(title=feature1.title, clients=feature1.clients, priority=feature1.priority).exists())
+        self.assertRaises(Exception, FeatureFactory.create(), title='A', clients='Client B', priority=1)
--- app/tests/test_services.py
+++ app/tests/test_services.py
@@ -35,3 +35,47 @@ class FeatureServicesTestCase(TestCase):
         update_other_priorities(self.feature[2])
         features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
         self.assertEqual(list(features_priorities), [1, 2, 66])
+
+    def test_that_creating_a_feature_with_an_existing_priority_bumps_other_features_with_same_or_higher_priorities(
+            self):
+        features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
+        self.assertListEqual(list(features_priorities), [1, 65, 92])
+
+        # create a new feature with priority 92 and note that self.feature[2] is currently 92
+        new_feature = FeatureFactory.create(priority=92)
+
+        update_other_priorities(new_feature)
+        features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
+        self.assertEqual(list(features_priorities), [1, 65, 92, 93])
+
+    def test_that_updating_a_feature_with_an_existing_priority_bumps_other_features_with_same_or_higher_priorities(
+            self):
+        features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
+        self.assertListEqual(list(features_priorities), [1, 65, 92])
+
+        self.feature[1].priority = 92  # note that self.feature[2] is currently 92
+        self.feature[1].save()
+
+        update_other_priorities(self.feature[1])
+        features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
+        self.assertEqual(list(features_priorities), [1, 92, 93])
+
+    def test_that_creating_two_new_features_and_updating_an_existing_feature_with_successive_priorities(self):
+        features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
+        self.assertListEqual(list(features_priorities), [1, 65, 92])
+
+        # create a new feature with priority 67
+        new_feature1 = FeatureFactory.create(priority=67)
+        update_other_priorities(new_feature1)
+
+        # change an existing feature to priority 66
+        self.feature[1].priority = 66
+        self.feature[1].save()
+        update_other_priorities(self.feature[1])
+
+        # create a new feature with priority 66
+        new_feature2 = FeatureFactory.create(priority=66)
+        update_other_priorities(new_feature2)
+
+        features_priorities = Feature.objects.all().values_list('priority', flat=True).order_by('priority')
+        self.assertEqual(list(features_priorities), [1, 66, 67, 69, 95])
