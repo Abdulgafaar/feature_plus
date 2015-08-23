58988694c760f39af1ce97b30426169e5271cb83
Muizudeen Kusimo <devboyng@gmail.com>
2015-08-23 14:02:31 -0400

    Update logic for admin.py

--- app/admin.py
+++ app/admin.py
@@ -7,9 +7,7 @@ class FeatureAdmin(admin.ModelAdmin):
     model = Feature
 
     def save_model(self, request, obj, form, change):
-        if obj.priority == PRIORITY_ONE:
-            update_other_priorities(obj)
-            super(FeatureAdmin, self).save_model(request, obj, form, change)
-
+        update_other_priorities(obj, **form.cleaned_data)
+        super(FeatureAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(Feature, FeatureAdmin)
